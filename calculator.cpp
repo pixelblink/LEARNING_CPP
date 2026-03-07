fa94af3711fbd108281a1ca9691d803dfae2c85a
pixelblink <chandandhare98@gmail.com>
2026-03-07 08:54:39 +0530

    basic arithmetic calculator

---
+++ calculator.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float a;
+    cout<<"enter 1st number";
+    cin>>a;
+    float b;
+    cout<<"enter 2nd number";
+    cin>>b;
+     char op;
+     cout<<"+ - * /";
+     cin>>op;
+     
+     if (op=='+')
+     {
+        cout<<a+b;
+     }
+     else if (op=='-')
+     {
+        cout<<a-b;
+     }
+     else if (op=='*')
+     {
+        cout<<a*b;
+     }
+     else if (op=='/')
+     {
+        cout<<a/b;
+     }
+     else{
+        cout<<"invalid input daalne wala gay";
+     }
+
+
+
+
+
+
+
+    return 0;
+}
