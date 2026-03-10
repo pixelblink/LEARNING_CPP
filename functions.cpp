f2759d8760e1760c5bda3bdf87b7ebd5528076cb
pixelblink <chandandhare98@gmail.com>
2026-03-10 20:00:59 +0530

    functions

---
+++ functions.cpp
@@ -0,0 +1,38 @@
+#inckude <iostream>
+using namespace std;
+
+// non return type without argument
+void hello(){
+    cout<<"hello"<<endl;
+}
+
+// non return type with argument
+void sum(int a, int b){
+    cout<<a+b;
+}
+
+
+// return type without argument
+string hello(){
+    return "helloooo";
+}
+
+// return type with argument
+int sum(int a, int b){
+    return a+b;
+}
+
+
+
+
+int main(){
+
+    int num1, num2;
+    cout<<"enter first number";
+    cin>>num1;
+    cout<<"enter second number";
+    cin>>num2;
+    sum(num1,num2);
+
+    return 0;
+}
