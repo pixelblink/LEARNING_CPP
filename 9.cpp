b0ac5ce5aad70a858babc8ace3782f5cb3d5a005
pixelblink <chandandhare98@gmail.com>
2026-03-20 02:22:36 +0530

    oops9

---
+++ 9.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+// ===================POLYMORPHISM===================
+// poly means "many" and morphism means "forms"
+
+// Compiletime(Static binding)=>> Operator overloading, Function overloading, Create normal object
+
+// Runtime(Synamic binding)=>> Function Overriding, Virtual keyword,Pure virtual keyword, Create pointer object
